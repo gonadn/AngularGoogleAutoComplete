8f1552eaa0fcf0ca274472a03bb19c45a555fca4
Adnan Riaz Gondal <gonadn@gmail.com>
2021-05-20 16:26:44 +0200

    Working

--- projects/testapp/src/app/app.module.ts
+++ projects/testapp/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AngularGoogleAutocompleteModule } from 'angular-google-autocomplete';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularGoogleAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
